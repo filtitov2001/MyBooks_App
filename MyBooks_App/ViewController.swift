52e9a5daf18e9171d0b99be4d059290312bb2e90
Felix Titov <titow.feliks@yandex.ru>
2022-07-02 15:10:45 +0300

    Initial Commit

---
+++ MyBooks_App/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MyBooks_App
+//
+//  Created by Felix Titov on 7/2/22.
+//  Copyright © 2022 by Felix Titov. All rights reserved.
+//  
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
