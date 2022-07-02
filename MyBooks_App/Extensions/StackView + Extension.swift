a1ab83a196df34da6ffa48e309c88c53851f68c7
Felix Titov <titow.feliks@yandex.ru>
2022-07-02 15:19:21 +0300

    Created logic of iOS App

---
+++ MyBooks_App/Extensions/StackView + Extension.swift
@@ -0,0 +1,18 @@
+//
+//  StackView + Extension.swift
+//  Pocket Reader
+//
+//  Created by Алексей Пархоменко on 26.01.2020.
+//  Copyright © 2020 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+extension UIStackView {
+    
+    convenience init(arrangedSubviews: [UIView], axis: NSLayoutConstraint.Axis, spacing: CGFloat) {
+        self.init(arrangedSubviews: arrangedSubviews)
+        self.axis = axis
+        self.spacing = spacing
+    }
+}
